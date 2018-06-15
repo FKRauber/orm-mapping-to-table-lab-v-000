c8e4caf7163deb91b30f69686fdac9b656bcb0dd
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-06-15 21:09:23 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,11 +7,7 @@ class Student
   attr_reader :id
 
   def initialize (name, grade, id=nil)
-  #  @id, @name, @grade = id, name, grade
-    @id = id,
-    @name = name,
-    @grade = grade
-
+    @id, @name, @grade = id, name, grade
   end
 
   def self.create_table
